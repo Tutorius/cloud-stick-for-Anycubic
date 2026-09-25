3ebb15f250a2d7dc1130770f7952fa5e019dfd23
Tutorius <85613003+Tutorius@users.noreply.github.com>
2026-09-25 07:18:30 +0200

    Modify config settings for TFT and functions
    Updated configuration settings for TFT rotation, delay for restart, and enabled restart and delete-all functions.

--- include/config.h
+++ include/config.h
@@ -1,3 +1,17 @@
+/*
+** Cloud-Stick for Anycubic
+** 
+** Set the "#define TFT_ROTATION" to 1 (normal mode, stick pointing to left) or 3 (stick pointing to right). For Anycubic Kobra 4, the 3 is correct choose
+**
+** Set "#define DELAY_FOR_RESTART" to a value in Milliseconds, values lower 1000 disable automatic restart after download. Other values lets wait the value in ms before restarting
+**
+** With the  "#define ENABLE_RESTART" and "#define ENABLE_DELETEALL" active the named dunctions work
+** RESTART: copying a file named "restart.cmd" to the Webdav-server restarts the stick after the waittime
+** DELETEALL : *** caution : deletes all files on stick AND Webdav-server. copying a file named "deleteall.cmd" starts this function. Use it carefully, if you know that you want it
+**
+** "#define NOUPLOAD" : the stick can not upload files to the Webdav-server
+*/
+
 #pragma once
 
 #include <Arduino.h>
@@ -19,8 +33,16 @@
 #define LED_DI_PIN     40
 #define LED_CI_PIN     39
 
-// TFT-Rotation
+// TFT-Rotation ( 1 for "normal" operation (stick pointing to the left), 3 for stick pointing to the right )
 #define TFT_ROTATION 3
 
-// DELAY_FOR_RESTART
-#define DELAY_FOR_RESTART 5000
+// DELAY_FOR_RESTART after download in ms, a value lower than 1000ms disables the function of automatical restart
+#define DELAY_FOR_RESTART 10000
+
+// Enable the Restart and Delete-all-function, comment it out to disable it
+#define ENABLE_RESTART 1
+#define ENABLE_DELETEALL 1
+
+// Disable Upload-function (Stick to WebDAV-server), uncomment to enable it
+// 3D-printers normally don't put things on a stick...
+#define NOUPLOAD 1
